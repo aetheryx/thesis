d8a518d80862ec38e91446fca01a7f2b3ce53615
aetheryx <mhzy.ali@gmail.com>
2025-06-29 21:46:36 +0200

    graphs: add notebooks

---
+++ graphs/src/graphs/shared.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+def init_plt():
+    plt.rcParams['font.family'] = 'Arial'
+    plt.rcParams['font.size'] = 12
+
+colors = {
+    'pd': '#4097ff',
+    'hd': '#2fbd57',
+}
